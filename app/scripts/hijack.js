a7da6419421aac651c0f1d2331e2d47b681678bc
Jasper Palfree <well.caffeinated@gmail.com>
2012-11-16 15:39:24 -0500

    incorporate youtube iframe api

--- app/scripts/hijack.js
+++ app/scripts/hijack.js
@@ -1,17 +1,29 @@
 require([
+    'require',
 	'modules/bonzo',
     'modules/shifty'
 ],
 function(
+    req,
 	bonzo,
     Tween
 ){
+    function loadYTApi(){
+        var tag = document.createElement('script');
+        tag.src = "//www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
 
     function fallOver( el, callback ){
 
-        var tweenable = new Tween();
+        var tweenable = new Tween()
+            ;
+
+        el = bonzo(el);
 
         bonzo(el.parent()).css({
+            'background': 'black',
             '-webkit-perspective': 400,
             '-moz-perspective': 400,
             '-ms-perspective': 400,
@@ -19,7 +31,7 @@ function(
         });
 
         el.css({
-
+            'zIndex': 1,
             'transform-origin': 'bottom center'
         });
 
@@ -83,16 +95,19 @@ function(
             callback: function(){
 
                 projectile.hide();
+                var img = shatterImg( target.parent() );
                 
-                fallOver( target, cb );                
+                setTimeout(function(){
+                    fallOver( bonzo([target[0], img[0]]), cb );
+                }, 2000);
             }
         });
     }
 
     function injectVideo( parent, ytid, callback ){
 
-        var injected = bonzo(bonzo.create('<div><iframe id="mp-frame" width="100%" height="100%" src="http://www.youtube.com/embed/'+ytid+'?autoplay=1" frameborder="0" allowfullscreen></iframe></div>'));
-
+        var injected = bonzo(bonzo.create('<div><div id="mp-player"></div></div>'));
+        
         injected.css({
             width: '100%',
             height: '100%',
@@ -104,12 +119,21 @@ function(
             zIndex: 0
         }).appendTo(parent);
 
-        document.getElementById('mp-frame').onload = function(){
-            callback(injected);
-        };
+        var player = new YT.Player('mp-player', {
+            height: '100%',
+            width: '100%',
+            videoId: ytid,
+            events: {
+                'onReady': function(evt){
+                    callback(injected, evt.target);
+                }
+            } 
+        });
     }
 
     function swingIn( el ){
+
+        el.css('zIndex', 5);
 
         var tweenable = new Tween();
         tweenable.tween({
@@ -128,29 +152,57 @@ function(
         });
     }
 
-    /*
-    start
-     */
-    var ytid = 'dmX1W5umC1c'
-        ,video = bonzo(document.getElementById('movie_player'))
-        ;
+    function shatterImg( par ){
 
-    bonzo(video.parent()).css({
-        position: 'relative'
-    });
+        var img = bonzo(bonzo.create('<div>'));
+        
+        img.css({
+            background: 'url('+ req.toUrl('.') +'/../images/shatter.png) no-repeat center center',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 4
+        }).appendTo(par);
 
-    video.css('zIndex', 2);
+        return img;
+    }
 
-    injectVideo( video.parent(), ytid, function( mp ){
+    window.onYouTubeIframeAPIReady = function(){
 
-        breakIt(video, function(){
+        /*
+        start
+         */
+        var ytid = 'dmX1W5umC1c'
+            ,video = bonzo(document.getElementById('movie_player'))
+            ,parent = video.parent() //document.getElementById('watch7-video')
+            ;
 
-            swingIn( mp );
-
-            var api = window.yt && window.yt.config_['PLAYER_REFERENCE'];
-            api && api.stopVideo();
-            video.css('display', 'none');
+        bonzo(parent).css({
+            position: 'relative',
+            overflow: 'hidden'
         });
-    });
+
+        video.css('zIndex', 2);
+
+        injectVideo( parent, ytid, function( mp, player ){
+
+            setTimeout(function(){
+                player.playVideo();
+            }, 2000);
+
+            breakIt(video, function(){
+
+                swingIn( mp );
+
+                var api = window.yt && window.yt.config_['PLAYER_REFERENCE'];
+                api && api.stopVideo();
+                video.css('display', 'none');
+            });
+        });
+    };
+
+    loadYTApi();
 	
 });
